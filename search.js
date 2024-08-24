5c287f944bb801c220b72b8608e5952affe21552
kuronekony4n <willynash99@gmail.com>
2024-08-24 16:52:30 +0700

    update

--- search.js
+++ search.js
@@ -1,15 +1,15 @@
 const searchInput = document.getElementById("search");
-const animeList = document.getElementById("resultContainer");
+const iconList = document.getElementById("resultContainer");
 
-const searchanime = async (searchBox) => {
+const searchicons = async (searchBox) => {
     try {
-        const res = await fetch("assets/file_list.json");
+        const res = await fetch("assets/file_list-1.21.json");
         if (!res.ok) {
             throw new Error(`Failed to fetch JSON: ${res.status} - ${res.statusText}`);
         }
-        const animedata = await res.json();
+        const icondata = await res.json();
 
-        let fits = animedata.icons.filter((icon) => {
+        let fits = icondata.icons.filter((icon) => {
             const regex = new RegExp(searchBox, "gi"); // Removed the "^" to match anywhere in the string
             return icon.match(regex);
         });
@@ -31,18 +31,18 @@ const outputHtml = (fits) => {
                 (icon) =>
                     `
                     <div class="inventory-item">
-                    <a href="https://raw.githubusercontent.com/kuronekony4n/mcicons/main/icons4/${icon}" download><img src="https://raw.githubusercontent.com/kuronekony4n/mcicons/main/icons4-thumb/${icon}" title="${icon}"></a>
+                    <a href="icons-1.21/${icon}" download target="_blank"><img src="icons-1.21-thumb/${icon}" title="${icon}"></a>
                     </div>
                 `
             )
             .join("");
-        animeList.innerHTML = iconFits;
+        iconList.innerHTML = iconFits;
     }
 };
 
-searchInput.addEventListener("input", () => searchanime(search.value));
+searchInput.addEventListener("input", () => searchicons(search.value));
 
 // Load all icons on page load
 window.addEventListener("load", () => {
-    searchanime("");
+    searchicons("");
 });
